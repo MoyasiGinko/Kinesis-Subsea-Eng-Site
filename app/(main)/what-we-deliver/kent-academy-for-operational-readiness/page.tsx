3ce6e809d23e782710d6169645138b5110970a8b
moyasiginko <mrsiyam61@gmail.com>
2025-06-29 03:26:02 +0600

    fix the name

--- app/(main)/what-we-deliver/kent-academy-for-operational-readiness/page.tsx
+++ app/(main)/what-we-deliver/kent-academy-for-operational-readiness/page.tsx
@@ -1,4 +1,4 @@
-import KinesisAcademy from "@/components/what-we-deliver/Kinesis-academy-for-operational-readiness";
+import KinesisAcademy from "@/components/what-we-deliver/kinesis-academy-for-operational-readiness";
 
 export default function Page() {
   return <KinesisAcademy />;
