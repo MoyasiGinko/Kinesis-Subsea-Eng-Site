eda1dbb95434e5d7792d64df7c4aa3e0d7922ac5
moyasiginko <mrsiyam61@gmail.com>
2025-06-29 02:33:37 +0600

    re-organize codebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import HeroBanner from './components/HeroBanner';
-import ServicesSection from './components/ServicesSection';
-import StatsSection from './components/StatsSection';
-import NewsSection from './components/NewsSection';
-import ContactSection from './components/ContactSection';
+import HeroBanner from "@/components/HeroBanner";
+import ServicesSection from "@/components/ServicesSection";
+import StatsSection from "@/components/StatsSection";
+import NewsSection from "@/components/NewsSection";
+import ContactSection from "@/components/ContactSection";
 
 export default function Home() {
   return (
