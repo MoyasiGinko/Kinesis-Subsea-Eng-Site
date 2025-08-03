538bd2431e1a557aba41f66d3cee0d37cb90a613
moyasiginko <mrsiyam61@gmail.com>
2025-08-03 14:07:17 +0600

    add new section

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,11 +3,13 @@ import ServicesSection from "@/components/common/ServicesSection";
 import StatsSection from "@/components/common/StatsSection";
 import NewsSection from "@/components/common/NewsSection";
 import ContactSection from "@/components/common/ContactSection";
+import ThreeSectorLayout from "@/components/common/SectorSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-br from-slate-800 via-slate-950 to-slate-900">
       <HeroBanner />
+      <ThreeSectorLayout />
       <ServicesSection />
       {/* <StatsSection /> */}
       <NewsSection />
