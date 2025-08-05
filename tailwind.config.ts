6b9b1a569a1bd0a5d3f1a7f32271e73f8bd261d8
moyasiginko <mrsiyam61@gmail.com>
2025-08-05 15:17:26 +0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,34 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#000000',
-        secondary: '#ffffff',
+        primary: "#000000",
+        secondary: "#ffffff",
+        "primary-blue": "#005EB8",
+        "primary-blue-hover": "#024690",
+        "primary-orange": "#FC5220",
+        "primary-orange-hover": "#e04b1c",
+        "primary-yellow": "#FDC900",
+        "primary-yellow-hover": "#e0b800",
       },
       fontFamily: {
-        sans: ['var(--font-geist-sans)'],
-        mono: ['var(--font-geist-mono)'],
+        sans: ["var(--font-geist-sans)"],
+        mono: ["var(--font-geist-mono)"],
       },
       maxWidth: {
-        '7xl': '80rem',
+        "7xl": "80rem",
       },
     },
   },
   plugins: [],
-  darkMode: 'class'
-}
+  darkMode: "class",
+};
 
-export default config
+export default config;
